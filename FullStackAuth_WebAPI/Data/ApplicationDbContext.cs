11f396c89f5c6c47d8e6258192588782c47ca247
Cody <cemt5462@gmail.com>
2023-07-24 15:25:57 -0500

    Created DTO files + created review controller with POST and tested. Made migration for favorites and reviews. Tested user registration and login.

--- FullStackAuth_WebAPI/Data/ApplicationDbContext.cs
+++ FullStackAuth_WebAPI/Data/ApplicationDbContext.cs
@@ -8,6 +8,9 @@ namespace FullStackAuth_WebAPI.Data
     public class ApplicationDbContext : IdentityDbContext<User>
     {
         public DbSet<Car> Cars { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+
 
         public ApplicationDbContext(DbContextOptions options)
     : base(options)
---
+++ FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class ReviewWithUserDTO
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Text { get; set; }
+        public string Rating { get; set; }
+        public UserForDisplayDto UserForDisplay { get; set; }
+    }
+}
